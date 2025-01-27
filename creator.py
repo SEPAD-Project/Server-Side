b937ed0a755b0edc1982fc4b498b82501d056115
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-27 17:14:26 +0330

    creating primary database and students table

---
+++ creator.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+
+db = mysql.connector.connect(
+    host="localhost",  
+    user="root",    
+    password="ardbms" 
+)
+
+cursor = db.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS school")
+print('Database Created')
+
+# connect to database school
+db.database = "sap"
+
+# creating table students
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        family VARCHAR(255),
+        national_code VARCHAR(255),
+        username VARCHAR(255),
+        password VARCHAR(255)
+    )
+""")
+
+# closing connection
+cursor.close()
+db.close()
+
+print("Table [students] Created")
+
+
+
