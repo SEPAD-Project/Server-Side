f9ae921698d2c4d0b6911f8fdbb201e7b97158e2
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-28 11:59:24 +0330

    database arg added to connect func

--- executer.py
+++ executer.py
@@ -6,9 +6,10 @@ try:
         host="localhost",
         user='root',
         password='ardbms',  # getpass("Enter password: ")
+        database='sap'
     ) as connection:
         print(connection)
-        create_db_query = "CREATE DATABASE sap"
+        create_db_query = "ALTER TABLE students ADD class VARCHAR(255)" # UPDATE `sap`.`students` SET `class` = '1052' WHERE (`id` = '1');
         with connection.cursor() as cursor:
             cursor.execute(create_db_query)
 except Error as e:
