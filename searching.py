d624d469b8b6080e6f94454be036328e1ad94178
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-29 22:21:07 +0330

    optimized for teachers on login page

--- searching.py
+++ searching.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def search_value(value, host='localhost', user='root', password='ardbms', database='sap'):
+def search_value(value, person, host='localhost', user='root', password='ardbms', database='sap'):
     # connetcting to db
     db = mysql.connector.connect(
         # host=host,  
@@ -11,28 +11,45 @@ def search_value(value, host='localhost', user='root', password='ardbms', databa
     user="root", # sapacc - ardbms
     port=5000,   
     password="sapprogram2583",
-    database='sap'
+    database=database
     )
     cursor = db.cursor()
-    cursor.execute("SELECT COUNT(*) FROM students WHERE username = %s", (value,))
-    result = cursor.fetchone()
-    if result[0] > 0:
-        cursor.execute('SELECT name, family, password, username, class, school, uid, national_code class FROM students WHERE username = %s', (value,))
-
+    if person == 'student' :
+        cursor.execute("SELECT COUNT(*) FROM students WHERE username = %s", (value,))
         result = cursor.fetchone()
-        cursor.close()
-        db.close()
+        if result[0] > 0:
+            cursor.execute('SELECT name, family, password, username, class, school, uid, national_code class FROM students WHERE username = %s', (value,))
 
-        if result:
-            return(result)
+            result = cursor.fetchone()
+            cursor.close()
+            db.close()
+
+            if result:
+                return(result)
+            else:
+                return(False)
         else:
-            return(False)
-    else:
-        cursor.close()
-        db.close()
-        return ('not found')
-
+            cursor.close()
+            db.close()
+            return ('not found')
+        
+    elif person == 'teacher' :
+        cursor.execute("SELECT COUNT(*) FROM teachers WHERE username = %s", (value,))
+        result = cursor.fetchone()
+        if result[0] > 0: # if user exist, return his/her information including password
+            cursor.execute('SELECT name, family, password FROM teachers WHERE username = %s', (value,))
+            result = cursor.fetchone()
+            cursor.close()
+            db.close()
+            if result:
+                return(result)
+            else:
+                return(False)
+        else:
+            cursor.close()
+            db.close()
+            return ('not found') # returning not found if user not exists
 
 if __name__ == '__main__' :
     x = input('value : ')
-    print(search_value(value=x))
+    print(search_value(value=x, person='student'))
