cfafcd3217cc4bc42c1ed44ff8c126f05dcc696b
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-02-25 11:41:57 +0330

    FTP Handler completed

--- ftp_server.py
+++ ftp_server.py
@@ -4,7 +4,36 @@ from pyftpdlib.servers import FTPServer
 import os
 
 class MyHandler(FTPHandler):
-    pass
+    def on_file_received(self, file):
+        try:
+            # getting recived file path
+            file_path = os.path.abspath(file)
+            
+            # extracting school code, class name, national code from filename
+            filename = os.path.basename(file_path)
+            filename, suffix = str(filename).split('.')
+            school_code, class_name, national_code = filename.split('_')[:3]
+            
+            # creating school directory if not exist
+            school_dir = os.path.join(os.getcwd(), school_code)
+            if not os.path.exists(school_dir):
+                os.makedirs(school_dir)
+            
+            # creating class directory if not exist 
+            class_dir = os.path.join(school_dir, class_name)
+            if not os.path.exists(class_dir):
+                os.makedirs(class_dir)
+            
+            # moving to class directory
+            new_file_path = os.path.join(class_dir, f"{national_code}.{suffix}")
+            if not os.path.exists(new_file_path):
+                os.rename(file_path, new_file_path)
+            else:
+                os.replace(file_path, new_file_path)
+
+            print(f"File <{filename}> uploaded successfully to <{new_file_path}>")
+        except Exception as e:
+            print(f"Error occured while uploaded file: {e}")
 
 def start_ftp_server():
     authorizer = DummyAuthorizer()
