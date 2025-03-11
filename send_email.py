504bdbf341acf4e361d3b8a2879fcaab72085b71
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-03-12 00:06:50 +0330

    Email sender completed

--- send_email.py
+++ send_email.py
@@ -1,27 +1,28 @@
-
 import smtplib
 from email.message import EmailMessage
 
 
 EMAIL_ADDRESS = "abolfazlrashidian94@gmail.com" 
-EMAIL_PASSWORD = ""   
+EMAIL_PASSWORD = "xaom tzfe lkxx gvgc"   
 
 
-receiver_email = input('enter your email : ')
+def send_mail(subject, content, receiver):
 
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = receiver
+    msg.set_content(content)
 
-msg = EmailMessage()
-msg["Subject"] = "test subject"
-msg["From"] = EMAIL_ADDRESS
-msg["To"] = receiver_email
-msg.set_content("test mail")
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:  
+            server.starttls() 
+            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            server.send_message(msg)
+        print('EMAIL has been Sent')
+    except Exception as e:
+        print(f"[ERROR] {e}")
 
-try:
-
-    with smtplib.SMTP("smtp.example.com", 587) as server:  #
-        server.starttls() 
-        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        server.send_message(msg)
-    print('sended ')
-except Exception as e:
-    print(f"Error {e}")
+if __name__ == "__name__":
+    receiver_email = input('enter your email : ')
+    send_mail('test', 'test content', receiver_email)
