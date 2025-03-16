ccd2b79526e95b4860b4d5152e1a341e4ebca859
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-03-16 23:59:35 +0330

    # Function to edit the name of the school directory created

--- directory_manager.py
+++ directory_manager.py
@@ -9,6 +9,13 @@ def create_school(school_name):
     except FileExistsError:
         print(f"School '{school_name}' already exists.")
 
+# Function to edit the name of the school directory
+def edit_school(old_school_name, new_school_name):
+    try:
+        os.rename(old_school_name, new_school_name)
+        print(f"School renamed from '{old_school_name}' to '{new_school_name}' successfully.")
+    except FileNotFoundError:
+        print(f"School '{old_school_name}' does not exist.")
 
 
 
