5780886bcfefe1513c9c922e7d521539ab0cc1cd
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-03-17 16:20:40 +0330

    base path added

--- log_handler.py
+++ log_handler.py
@@ -1,9 +1,11 @@
 import time
 
-def log_message(message, filename):
+BASE_PATH = "C://sap-project//log.txt"
+
+def log_message(message, filename=BASE_PATH):
     formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
     with open(filename, 'a') as file:
         file.write(f"[{formatted_time}] {message}\n")
 
 if __name__ == '__main__':
-    log_message('this is test log', 'log.txt')
+    log_message('this is test log')
