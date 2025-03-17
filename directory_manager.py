3f5d0ac7f17adbd14c759266f18d7ede5e1e539b
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-03-17 15:08:13 +0330

    Base path added, error handling improved, some names changed

--- directory_manager.py
+++ directory_manager.py
@@ -1,112 +1,142 @@
 import os
 import json
 
-# Function to create a school directory
-def create_school(school_name):
-    try:
-        os.mkdir(school_name)
-        print(f"School '{school_name}' created successfully.")
-    except FileExistsError:
-        print(f"School '{school_name}' already exists.")
+# Define the base path where all schools will be created
+BASE_PATH = "C://sap-project//schools"
 
-# Function to edit the name of the school directory
-def edit_school(old_school_name, new_school_name):
+# Ensure the base path exists
+if not os.path.exists(BASE_PATH):
+    os.makedirs(BASE_PATH)
+
+# Function to create a school directory
+def create_school(school_code):
+    school_path = os.path.join(BASE_PATH, school_code)
     try:
-        os.rename(old_school_name, new_school_name)
-        print(f"School renamed from '{old_school_name}' to '{new_school_name}' successfully.")
+        os.mkdir(school_path)
+        print(f"School '{school_code}' created successfully at '{school_path}'.")
+    except FileExistsError:
+        print(f"School '{school_code}' already exists at '{school_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create school '{school_code}' at '{school_path}'.")
+
+# Function to edit the code of the school directory
+def edit_school(old_school_code, new_school_code):
+    old_school_path = os.path.join(BASE_PATH, old_school_code)
+    new_school_path = os.path.join(BASE_PATH, new_school_code)
+    try:
+        os.rename(old_school_path, new_school_path)
+        print(f"School renamed from '{old_school_code}' to '{new_school_code}' successfully.")
     except FileNotFoundError:
-        print(f"School '{old_school_name}' does not exist.")
+        print(f"School '{old_school_code}' does not exist at '{old_school_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to rename school '{old_school_code}' to '{new_school_code}'.")
 
 # Function to delete the school directory
-def delete_school(school_name):
+def delete_school(school_code):
+    school_path = os.path.join(BASE_PATH, school_code)
     try:
-        os.rmdir(school_name)
-        print(f"School '{school_name}' deleted successfully.")
+        os.rmdir(school_path)
+        print(f"School '{school_code}' deleted successfully from '{school_path}'.")
     except FileNotFoundError:
-        print(f"School '{school_name}' does not exist.")
+        print(f"School '{school_code}' does not exist at '{school_path}'.")
     except OSError:
-        print(f"School '{school_name}' is not empty.")
+        print(f"School '{school_code}' is not empty at '{school_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to delete school '{school_code}' at '{school_path}'.")
 
 # Function to create a class directory inside the school directory
-def create_class(school_name, class_name):
-    class_path = os.path.join(school_name, class_name)
+def create_class(school_code, class_code):
+    class_path = os.path.join(BASE_PATH, school_code, class_code)
     try:
         os.mkdir(class_path)
-        print(f"Class '{class_name}' created in school '{school_name}' successfully.")
+        print(f"Class '{class_code}' created in school '{school_code}' successfully at '{class_path}'.")
     except FileExistsError:
-        print(f"Class '{class_name}' already exists in school '{school_name}'.")
+        print(f"Class '{class_code}' already exists in school '{school_code}' at '{class_path}'.")
     except FileNotFoundError:
-        print(f"School '{school_name}' does not exist.")
+        print(f"School '{school_code}' does not exist at '{os.path.join(BASE_PATH, school_code)}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create class '{class_code}' in school '{school_code}' at '{class_path}'.")
 
-# Function to edit the name of a class directory
-def edit_class(school_name, old_class_name, new_class_name):
-    old_class_path = os.path.join(school_name, old_class_name)
-    new_class_path = os.path.join(school_name, new_class_name)
+# Function to edit the code of a class directory
+def edit_class(school_code, old_class_code, new_class_code):
+    old_class_path = os.path.join(BASE_PATH, school_code, old_class_code)
+    new_class_path = os.path.join(BASE_PATH, school_code, new_class_code)
     try:
         os.rename(old_class_path, new_class_path)
-        print(f"Class renamed from '{old_class_name}' to '{new_class_name}' in school '{school_name}' successfully.")
+        print(f"Class renamed from '{old_class_code}' to '{new_class_code}' in school '{school_code}' successfully.")
     except FileNotFoundError:
-        print(f"Class '{old_class_name}' does not exist in school '{school_name}'.")
+        print(f"Class '{old_class_code}' does not exist in school '{school_code}' at '{old_class_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to rename class '{old_class_code}' to '{new_class_code}' in school '{school_code}'.")
 
 # Function to delete a class directory
-def delete_class(school_name, class_name):
-    class_path = os.path.join(school_name, class_name)
+def delete_class(school_code, class_code):
+    class_path = os.path.join(BASE_PATH, school_code, class_code)
     try:
         os.rmdir(class_path)
-        print(f"Class '{class_name}' deleted from school '{school_name}' successfully.")
+        print(f"Class '{class_code}' deleted from school '{school_code}' successfully at '{class_path}'.")
     except FileNotFoundError:
-        print(f"Class '{class_name}' does not exist in school '{school_name}'.")
+        print(f"Class '{class_code}' does not exist in school '{school_code}' at '{class_path}'.")
     except OSError:
-        print(f"Class '{class_name}' is not empty.")
+        print(f"Class '{class_code}' is not empty at '{class_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to delete class '{class_code}' in school '{school_code}' at '{class_path}'.")
 
 # Function to create a student with JSON and TXT files
-def create_student(school_name, class_name, student_name):
-    class_path = os.path.join(school_name, class_name)
-    student_json_path = os.path.join(class_path, f"{student_name}.json")
-    student_txt_path = os.path.join(class_path, f"{student_name}.txt")
+def create_student(school_code, class_code, student_code):
+    class_path = os.path.join(BASE_PATH, school_code, class_code)
+    student_json_path = os.path.join(class_path, f"{student_code}.json")
+    student_txt_path = os.path.join(class_path, f"{student_code}.txt")
     
     try:
         with open(student_json_path, 'w') as json_file:
-            json.dump({"name": student_name}, json_file)
+            json.dump({"name": student_code}, json_file)
         with open(student_txt_path, 'w') as txt_file:
-            txt_file.write(f"Student Name: {student_name}\n")
-        print(f"Student '{student_name}' created in class '{class_name}' of school '{school_name}' successfully.")
+            txt_file.write(f"Student Name: {student_code}\n")
+        print(f"Student '{student_code}' created in class '{class_code}' of school '{school_code}' successfully at '{class_path}'.")
     except FileNotFoundError:
-        print(f"Class '{class_name}' does not exist in school '{school_name}'.")
+        print(f"Class '{class_code}' does not exist in school '{school_code}' at '{class_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create student '{student_code}' in class '{class_code}' of school '{school_code}' at '{class_path}'.")
 
 # Function to edit the name of a student
-def edit_student(school_name, class_name, old_student_name, new_student_name):
-    class_path = os.path.join(school_name, class_name)
-    old_student_json_path = os.path.join(class_path, f"{old_student_name}.json")
-    old_student_txt_path = os.path.join(class_path, f"{old_student_name}.txt")
-    new_student_json_path = os.path.join(class_path, f"{new_student_name}.json")
-    new_student_txt_path = os.path.join(class_path, f"{new_student_name}.txt")
+def edit_student(school_code, class_code, old_student_code, new_student_code):
+    class_path = os.path.join(BASE_PATH, school_code, class_code)
+    old_student_json_path = os.path.join(class_path, f"{old_student_code}.json")
+    old_student_txt_path = os.path.join(class_path, f"{old_student_code}.txt")
+    new_student_json_path = os.path.join(class_path, f"{new_student_code}.json")
+    new_student_txt_path = os.path.join(class_path, f"{new_student_code}.txt")
     
     try:
         os.rename(old_student_json_path, new_student_json_path)
         os.rename(old_student_txt_path, new_student_txt_path)
-        print(f"Student renamed from '{old_student_name}' to '{new_student_name}' in class '{class_name}' of school '{school_name}' successfully.")
+        print(f"Student renamed from '{old_student_code}' to '{new_student_code}' in class '{class_code}' of school '{school_code}' successfully.")
     except FileNotFoundError:
-        print(f"Student '{old_student_name}' does not exist in class '{class_name}' of school '{school_name}'.")
+        print(f"Student '{old_student_code}' does not exist in class '{class_code}' of school '{school_code}' at '{class_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to rename student '{old_student_code}' to '{new_student_code}' in class '{class_code}' of school '{school_code}'.")
 
 # Function to delete a student
-def delete_student(school_name, class_name, student_name):
-    class_path = os.path.join(school_name, class_name)
-    student_json_path = os.path.join(class_path, f"{student_name}.json")
-    student_txt_path = os.path.join(class_path, f"{student_name}.txt")
+def delete_student(school_code, class_code, student_code):
+    class_path = os.path.join(BASE_PATH, school_code, class_code)
+    student_json_path = os.path.join(class_path, f"{student_code}.json")
+    student_txt_path = os.path.join(class_path, f"{student_code}.txt")
     
     try:
         os.remove(student_json_path)
         os.remove(student_txt_path)
-        print(f"Student '{student_name}' deleted from class '{class_name}' of school '{school_name}' successfully.")
+        print(f"Student '{student_code}' deleted from class '{class_code}' of school '{school_code}' successfully at '{class_path}'.")
     except FileNotFoundError:
-        print(f"Student '{student_name}' does not exist in class '{class_name}' of school '{school_name}'.")
+        print(f"Student '{student_code}' does not exist in class '{class_code}' of school '{school_code}' at '{class_path}'.")
+    except PermissionError:
+        print(f"Permission denied: Unable to delete student '{student_code}' in class '{class_code}' of school '{school_code}' at '{class_path}'.")
 
 # Example usage
-create_school("MySchool")
-create_class("MySchool", "ClassA")
-create_student("MySchool", "ClassA", "JohnDoe")
-edit_student("MySchool", "ClassA", "JohnDoe", "JaneDoe")
-delete_student("MySchool", "ClassA", "JaneDoe")
-delete_class("MySchool", "ClassA")
-delete_school("MySchool")
+if __name__ == "__main__":
+    create_school("MySchool")
+    create_class("MySchool", "ClassA")
+    create_student("MySchool", "ClassA", "JohnDoe")
+    edit_student("MySchool", "ClassA", "JohnDoe", "JaneDoe")
+    delete_student("MySchool", "ClassA", "JaneDoe")
+    delete_class("MySchool", "ClassA")
+    delete_school("MySchool")
