3c756d318e4759e4c8f751dcc6014ccedadf4a16
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-03-25 18:06:37 +0330

    debug mode turned off

--- frame_server.py
+++ frame_server.py
@@ -76,4 +76,4 @@ def get_student_image():
 
 if __name__ == '__main__':
     log_message(f"FRAME SERVER | Server started on port {port}")
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
