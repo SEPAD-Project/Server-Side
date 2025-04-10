8c1d3a6e1faf986cfb9f8e3232386255d81cc55a
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-04-10 23:59:59 +0330

    Database creator updated

--- Database/creator.py
+++ Database/creator.py
@@ -1,22 +1,20 @@
 import mysql.connector
 
-
 db = mysql.connector.connect(
     host="185.4.28.110",  
-    user="root", # sapacc - ardbms
+    user="root",
     port=5000,   
     password="sapprogram2583" 
 )
 
 cursor = db.cursor()
-
 cursor.execute("CREATE DATABASE IF NOT EXISTS sap")
-print('Database Created')
+print('"SAP" DATABASE CREATED.')
 
 # connect to database school
 db.database = "sap"
 
-# creating table students
+# Creating students table
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS students (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -28,8 +26,9 @@ cursor.execute("""
         school_code VARCHAR(255)
     )
 """)
-print("Table [students] Created")
+print('"STUDENTS" TABLE CREATED.')
 
+# Creating teachers table
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS teachers (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -40,13 +39,34 @@ cursor.execute("""
         teacher_classes VARCHAR(255)
     )
 """)
-print("Table [teachers] Created")
+print('"TEACHERS" TABLE CREATED.')
 
+# Creating classes table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS classes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        class_name VARCHAR(255),
+        class_code VARCHAR(255),
+        school_code VARCHAR(255),
+        teachers VARCHAR(255)
+    )
+""")
+print('"CLASSES" TABLE CREATED.')
+
+# Creating schools table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS teachers (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        school_name VARCHAR(255),
+        school_code VARCHAR(255),
+        manager_personal_code VARCHAR(255),
+        province VARCHAR(255),
+        city VARCHAR(255),
+        teachers VARCHAR(255)
+    )
+""")
+print('"SCHOOLS" TABLE CREATED.')
 
 # closing connection
 cursor.close()
 db.close()
-
-
-
-
