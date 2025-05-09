2c3ddaf111046ca54ffc48f4db215e9b1ba2be6b
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-05-09 19:50:08 +0330

    admins table added

--- Database/creator.py
+++ Database/creator.py
@@ -15,57 +15,68 @@ print('"SAP" DATABASE CREATED.')
 db.database = "sap"
 
 # Creating students table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS students (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        student_name VARCHAR(255),
-        student_family VARCHAR(255),
-        student_national_code VARCHAR(255),
-        student_password VARCHAR(255),
-        class_code VARCHAR(255),
-        school_code VARCHAR(255)
-    )
-""")
-print('"STUDENTS" TABLE CREATED.')
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS students (
+#         id INT AUTO_INCREMENT PRIMARY KEY,
+#         student_name VARCHAR(255),
+#         student_family VARCHAR(255),
+#         student_national_code VARCHAR(255),
+#         student_password VARCHAR(255),
+#         class_code VARCHAR(255),
+#         school_code VARCHAR(255)
+#     )
+# """)
+# print('"STUDENTS" TABLE CREATED.')
 
 # Creating teachers table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS teachers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        teacher_name VARCHAR(255),
-        teacher_family VARCHAR(255),
-        teacher_national_code VARCHAR(255),
-        teacher_password VARCHAR(255),
-        teacher_classes VARCHAR(255)
-    )
-""")
-print('"TEACHERS" TABLE CREATED.')
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS teachers (
+#         id INT AUTO_INCREMENT PRIMARY KEY,
+#         teacher_name VARCHAR(255),
+#         teacher_family VARCHAR(255),
+#         teacher_national_code VARCHAR(255),
+#         teacher_password VARCHAR(255),
+#         teacher_classes VARCHAR(255),
+#         lesson VARCHAR(255)
+#     )
+# """)
+# print('"TEACHERS" TABLE CREATED.')
 
 # Creating classes table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS classes (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        class_name VARCHAR(255),
-        class_code VARCHAR(255),
-        school_code VARCHAR(255),
-        teachers VARCHAR(255)
-    )
-""")
-print('"CLASSES" TABLE CREATED.')
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS classes (
+#         id INT AUTO_INCREMENT PRIMARY KEY,
+#         class_name VARCHAR(255),
+#         class_code VARCHAR(255),
+#         school_code VARCHAR(255),
+#         teachers VARCHAR(255)
+#     )
+# """)
+# print('"CLASSES" TABLE CREATED.')
 
 # Creating schools table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS teachers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        school_name VARCHAR(255),
-        school_code VARCHAR(255),
-        manager_personal_code VARCHAR(255),
-        province VARCHAR(255),
-        city VARCHAR(255),
-        teachers VARCHAR(255)
-    )
-""")
-print('"SCHOOLS" TABLE CREATED.')
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS schools (
+#         id INT AUTO_INCREMENT PRIMARY KEY,
+#         school_name VARCHAR(255),
+#         school_code VARCHAR(255),
+#         manager_personal_code VARCHAR(255),
+#         province VARCHAR(255),
+#         city VARCHAR(255),
+#         teachers VARCHAR(255),
+#         email VARCHAR(255)               
+#     )
+# """)
+# print('"SCHOOLS" TABLE CREATED.')
+
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS admins (
+#         id INT AUTO_INCREMENT PRIMARY KEY,
+#         username VARCHAR(255),
+#         password VARCHAR(255)
+#     )
+# """)
+# print('"admins" TABLE CREATED.')
 
 # closing connection
 cursor.close()
