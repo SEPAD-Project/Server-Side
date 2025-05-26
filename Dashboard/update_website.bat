b63736912a819b0456bf49a02d0b8686ba6bf3f5
parsasafaie <parsasafaie.2568@proton.me>
2025-05-26 07:51:23 +0330

    update webiste webhook .bat file and add celery

--- Dashboard/update_website.bat
+++ Dashboard/update_website.bat
@@ -4,4 +4,6 @@ git fetch --all
 git reset --hard origin/main
 git clean -fd
 taskkill /IM waitress-serve.exe /F
+taskkill /IM celery.exe /F
+start "Celery Worker" cmd /k "cd C:/Users/Administrator/Documents/web && .venv\Scripts\activate && celery -A source.celery worker --loglevel=info -P threads"
 waitress-serve --listen=0.0.0.0:2568 source.app:app
