b4fadb00c341c9038e2af97b3bdc5fc9730f5698
parsasafaie <parsasafaie.2568@proton.me>
2025-06-17 13:18:11 +0330

    website webhook updated for new website repo name

--- Dashboard/update_website.bat
+++ Dashboard/update_website.bat
@@ -3,7 +3,7 @@ echo ============================================================
 echo ^>^>^> Starting Web Update Process
 echo ============================================================
 
-cd /d C:\Users\Administrator\Documents\web
+cd /d C:\Users\Administrator\Documents\WebApplication
 
 echo ^>^>^> Fetching latest code from Git
 git fetch --all
@@ -23,11 +23,11 @@ taskkill /FI "WINDOWTITLE eq Flask Server" /F >nul 2>&1
 
 echo.
 echo ^>^>^> Launching new Celery Worker
-start "Celery Worker" cmd /k "cd /d C:\Users\Administrator\Documents\web && .venv\Scripts\activate && python -m celery -A source.celery worker --loglevel=info -P threads"
+start "Celery Worker" cmd /k "cd /d C:\Users\Administrator\Documents\WebApplication && .venv\Scripts\activate && python -m celery -A source.celery worker --loglevel=info -P threads"
 
 echo.
 echo ^>^>^> Launching Flask Server with Waitress
-start "Flask Server" cmd /k "cd /d C:\Users\Administrator\Documents\web && .venv\Scripts\activate && waitress-serve --listen=0.0.0.0:2568 source.app:app"
+start "Flask Server" cmd /k "cd /d C:\Users\Administrator\Documents\WebApplication && .venv\Scripts\activate && waitress-serve --listen=0.0.0.0:2568 source.app:app"
 
 echo.
 echo ============================================================
