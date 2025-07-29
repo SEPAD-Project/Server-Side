45b7ac3ce53a39b06aac8c5fc83fa63f014a7ea0
parsasafaie <parsasafaie.2568@proton.me>
2025-07-29 17:02:08 +0330

    update website webhook for new django edition of website

--- Dashboard/update_website.bat
+++ Dashboard/update_website.bat
@@ -11,23 +11,12 @@ git reset --hard origin/main
 git clean -fd
 
 echo.
-echo ^>^>^> Terminating existing Celery and Flask processes...
-
-taskkill /IM waitress-serve.exe /F >nul 2>&1
-wmic process where "CommandLine like '%%celery%%' and name='python.exe'" call terminate >nul 2>&1
-wmic process where "CommandLine like '%%celery%%' and name='cmd.exe'" call terminate >nul 2>&1
-wmic process where "CommandLine like '%%waitress%%' and name='python.exe'" call terminate >nul 2>&1
-wmic process where "CommandLine like '%%waitress%%' and name='cmd.exe'" call terminate >nul 2>&1
-taskkill /FI "WINDOWTITLE eq Celery Worker" /F >nul 2>&1
-taskkill /FI "WINDOWTITLE eq Flask Server" /F >nul 2>&1
+echo ^>^>^> Terminating existing Django and Celery processes...
+call stop_server.bat
 
 echo.
-echo ^>^>^> Launching new Celery Worker
-start "Celery Worker" cmd /k "cd /d C:\Users\Administrator\Documents\WebApplication && .venv\Scripts\activate && python -m celery -A source.celery worker --loglevel=info -P threads"
-
-echo.
-echo ^>^>^> Launching Flask Server with Waitress
-start "Flask Server" cmd /k "cd /d C:\Users\Administrator\Documents\WebApplication && .venv\Scripts\activate && waitress-serve --listen=0.0.0.0:85 source.app:app"
+echo ^>^>^> Starting new Django and Celery processes
+call start_server.bat
 
 echo.
 echo ============================================================
